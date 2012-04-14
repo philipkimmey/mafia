5096c97d69e882881dcb2245cd7bef9db4981acb
Philip Kimmey <philip.kimmey@gmail.com>
2012-04-14 15:08:20 -0700

    [settings] setup static files urls

--- mafia/settings.py
+++ mafia/settings.py
@@ -67,9 +67,7 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(BASE_PATH, 'mafia', 'static'),
 )
 
 # List of finder classes that know how to find static files in
