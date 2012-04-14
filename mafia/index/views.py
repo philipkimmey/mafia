177908d09fec5241ac4b39de13bda5104fefb8cb
Cosmo Smith <cosmo@rover.com>
2012-04-14 15:00:50 -0700

    [urls] more url/registration changes

--- mafia/index/views.py
+++ mafia/index/views.py
@@ -1,1 +1,12 @@
-# Create your views here.
+from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+
+def logout_page(request):
+    return HttpResponseRedirect('/')
+    
+def register_page(request):
+    return HttpResponseRedirect('/')
+    
+def index(request):
+    return render(request, 'index.html', {})
--- mafia/urls.py
+++ mafia/urls.py
@@ -1,4 +1,7 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic.simple import direct_to_template
+
+from index.views import *
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -6,12 +9,10 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     # Manage session
+    (r'^$', index),
     (r'^login/$', 'django.contrib.auth.views.login'),
     (r'^logout/$', logout_page),
     (r'^register/$', register_page),
     (r'^register/success/$', direct_to_template,
        {'template': 'registration/register_success.html'}),
-
-    url(r'^login/$', 'mafia.views.login'),
-    url(r'^games/', include('mafia.games.urls')),
 )
