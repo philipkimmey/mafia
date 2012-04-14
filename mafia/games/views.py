99b49df0634a905ea7dd51bc8ae22a929e074c17
Philip Kimmey <philip.kimmey@gmail.com>
2012-04-14 16:24:59 -0700

    [games] initial work on building a dashboard

--- mafia/games/views.py
+++ mafia/games/views.py
@@ -3,11 +3,17 @@ from django.template import RequestContext
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from django.shortcuts import render_to_response
+from django.shortcuts import get_object_or_404
 
 
 def new(request):
     return render(request, 'new.html', {})
 
+
 def init_game(request):
     return HttpResponse("html")
+
+
+def game(request, pk):
+    game = get_object_or_404(Game, pk=pk)
+    return render(request, 'games/game.html', {'game': game})
