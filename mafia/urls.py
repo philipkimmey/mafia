264f434b80f55c26f8b4a81fe6eeaed84f21977c
Cosmo Smith <cosmo@rover.com>
2012-04-14 14:08:45 -0700

    [urls] initial url structure

--- mafia/urls.py
+++ mafia/urls.py
@@ -5,13 +5,6 @@ from django.conf.urls import patterns, include, url
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'mafia.views.home', name='home'),
-    # url(r'^mafia/', include('mafia.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^login/$', 'mafia.views.login'),
+    url(r'^games/', include('mafia.games.urls')),
 )
