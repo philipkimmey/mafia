67908bc1b216450e35368f26ce239f3194f4604e
Philip Kimmey <philip.kimmey@gmail.com>
2012-04-14 16:34:28 -0700

    [games] remove unimportable votes view

--- mafia/games/urls.py
+++ mafia/games/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     url(r'^new/$', 'games.views.new'),
-    url(r'^(\d+)/$', 'games.views.game'),
-    url(r'^(\d+)/votes/$', 'games.views.votes'),
+    url(r'^(\d+)/$', 'games.views.game', name="game"),
+    #url(r'^(\d+)/votes/$', 'games.views.votes'),
 )
